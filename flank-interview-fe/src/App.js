344d8d7bbf4a53b21bb7ed967c45ae7b5103f368
Gabriella Bova <bova3.0@gmail.com>
2023-10-05 18:34:47 -0500

    Button selection logic

--- flank-interview-fe/src/App.js
+++ flank-interview-fe/src/App.js
@@ -14,8 +14,47 @@ const options = [
     { value: 500, label: '$500' },
 ]
 
+
 function App() {
     const [selected, setSelected] = useState([]);
+
+    function selectRangeOfOptions(selectedOptions) {
+        if (selectedOptions.length <= 1) {
+            return selectedOptions;
+        }
+
+        let max = selectedOptions[0].value;
+        let min = selectedOptions[0].value;
+        selectedOptions.forEach((option) => {
+            if (option.value > max) {
+                max = option.value;
+            } else if (option.value < min) {
+                min = option.value;
+            }
+        });
+
+        return options.filter(o => o.value >= min && o.value <= max);
+    }
+
+    function buttonClicked(option) {
+        let updatedSelected = selected;
+        if (selected.filter(o => o.value == option.value).length > 0) {
+            // If option already in selected, remove from selected
+            updatedSelected = selected.filter(o => o.value !== option.value);
+        } else if (option.value == null) {
+            // If option is ALL, clear selected and only keep ALL
+            updatedSelected = [option];
+        } else {
+            // Adding new option to selected
+            let allList = selected.filter(o => o.value == null);
+            if (selected.filter(o => o.value == null).length > 0) {
+                // Remove ALL if it's in selected 
+                updatedSelected = selected.filter(o => o.value !== null);
+            }
+            updatedSelected = selectRangeOfOptions([...updatedSelected, option])
+        }
+        setSelected(updatedSelected);
+    }
 
     return (
         <ThinTemplate instructionTitle="Front-end Interview">
@@ -24,6 +63,7 @@ function App() {
                     <button
                         type='button'
                         key={option.value}
+                        onClick={() => buttonClicked(option)}
                     >
                         {option.label}
                     </button>
